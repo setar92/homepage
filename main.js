6fe65dfd3f169c15a8a2ba53ee4c18afb9aabf54
setar1992 <setar1992@i.ua>
2022-06-06 13:36:35 +0300

    added moving block

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
-let avatar = document.querySelector(".contact-info__block");
-let resume__block = document.querySelector('.resume__block');
-let img = document.querySelector('.contact-info__block img');
 let toggle = true;
-console.log(resume__block);
 
-avatar.addEventListener('mouseover', () => {
-    if (toggle) {
-        resume__block.classList.add('reverse');
-        setTimeout(() => {
-            img.src = 'assets/avatar_mirror.png';
-        }, 1000)
-        
-    } else {
-        resume__block.classList.remove('reverse');
-        setTimeout(() => {
-            img.src = 'assets/avatar.png';
-        }, 1000)
-        
-    }
+function mirror() {
+    const avatar = document.querySelector(".contact-info__block");
+    const resume__block = document.querySelector('.resume__block');
+    const img = document.querySelector('.contact-info__block img');
+    const pashal = document.querySelector('.pashal');
 
-    toggle = !toggle
+    const resume = document.querySelector('.resume');
+    let coordResume = resume.getBoundingClientRect();
+    let pashalCoordinate = pashal.getBoundingClientRect();
 
-});
+    avatar.addEventListener('mouseover', () => {
+        let step = (coordResume.right - coordResume.left) / 100;
+        let start = 0;
+        if (toggle) {
+            resume__block.classList.add('reverse');
+            setTimeout(() => {
+                img.src = 'assets/avatar_mirror.png';
+            }, 500)
+
+
+            let timer = setInterval(() => {
+                pashal.style.marginRight = start + 'px';
+                start += step;
+                if (coordResume.left >= (coordResume.right - start)) {
+                    clearInterval(timer);
+                    pashal.style.marginRight = (coordResume.width - 11) + 'px';
+
+                }
+            }, 10);
+
+
+
+        } else {
+            start = coordResume.width;
+            resume__block.classList.remove('reverse');
+            setTimeout(() => {
+                img.src = 'assets/avatar.png';
+            }, 500)
+            let timer = setInterval(() => {
+                pashal.style.marginRight = start + 'px';
+                start -= step;
+                console.log(pashal.style.marginRight);
+                if (parseInt(pashal.style.marginRight) <= 0) {
+                    clearInterval(timer);
+                    pashal.style.marginRight = 0 + 'px'
+
+                }
+            }, 10);
+
+        }
+
+        toggle = !toggle
+    });
+}
+mirror();
+
